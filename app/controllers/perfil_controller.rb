1ded8940c638274b1c14a2c66cdc316cf943c481
Renan Siqueira <renan.dsiqueira@gmail.com>
2017-03-26 09:27:45 -0300

    update time_zone

--- app/controllers/perfil_controller.rb
+++ app/controllers/perfil_controller.rb
@@ -5,7 +5,13 @@ class PerfilController < ApplicationController
 		@current_user = current_user
 	end
 
+	# POST /perfil/update
 	def update
+		user_edit = params[:user]
+		time_zone = user_edit[:time_zone]
 
+		u = User.find(current_user.id)
+		u.time_zone = time_zone
+		u.save()
 	end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   def create
   	auth = request.env['omniauth.auth']
+    
   	unless @auth = Authorization.find_from_hash(auth)
     	# Create a new user or add an auth to existing user, depending on
     	# whether there is already a user signed in.
